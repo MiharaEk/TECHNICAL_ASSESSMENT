5a05abb1bda47a5533d128433fe994a2d520e536
MiharaEk <mipinuri98@gmail.com>
2022-10-29 12:06:48 +0530

    Backend Application

---
+++ backend/src/test/java/com/mihara/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihara.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
