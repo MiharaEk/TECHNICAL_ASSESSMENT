ddfd60b3944efd98f206ec2c1b1fbce027f33378
MiharaEk <mipinuri98@gmail.com>
2022-10-31 11:04:17 +0530

    Test

--- backend/src/test/java/com/mihara/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/mihara/backend/BackendApplicationTests.java
@@ -1,13 +1,21 @@
 package com.mihara.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.mihara.backend.Controller.TodoController;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+	@Autowired
+	private TodoController controller;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
